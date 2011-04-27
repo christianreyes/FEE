7fa0ffa76086296b0e1730c1156e035ae22d972b
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-04-27 00:01:34 -0400

    layout ready for all articles

--- interactions.js
+++ interactions.js
@@ -1,13 +1,22 @@
 $(function() {
+	$("#article-pictures li").hide();
+	$("#article-pictures li:first").show();
 	$("#article li").hide();
 	$("#article li:first").show();
 	
 	$("#content-sidebar li").click(function(event){
 	  event.preventDefault();
+	  
+	  $("#content-sidebar li").eq(id_selected).removeClass("selected");
 	  $("#article li:visible").fadeOut();
-	  var id = $("#content-sidebar li").index(this);
-	  $('#article li').eq(id).fadeIn();
+	  $("#article-pictures li:visible").hide();
+	  
+	  id_selected = $("#content-sidebar li").index(this);
+	  $('#article li').eq(id_selected).fadeIn();
+	  $("#article-pictures li").eq(id_selected).show();
+	  $(this).addClass("selected");
     });
 	
-	
 });
+
+var id_selected = -1;
