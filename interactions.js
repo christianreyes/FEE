410b08612b422d99fb3069895d00e7e502128653
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-04-26 23:06:46 -0400

    good javascript

--- interactions.js
+++ interactions.js
@@ -1,10 +1,12 @@
 $(function() {
 	$("#article li").hide();
 	$("#article li:first").show();
-
-	$("#content-sidebar li").click(function () {
-	  $("#article li:visible").hide();
-	  $('#article li').eq($("#content-sidebar li").index(this)).show();
+	
+	$("#content-sidebar li").click(function(event){
+	  event.preventDefault();
+	  $("#article li:visible").fadeOut();
+	  var id = $("#content-sidebar li").index(this);
+	  $('#article li').eq(id).fadeIn();
     });
 	
 	
